07e7fe70ef98fb810723aa42cc16236b8862828a
Martin Svalin <martin@lite.nu>
2015-03-06 16:19:51 +0100

    You know… another bug fixed

--- lib/guard/kjell.rb
+++ lib/guard/kjell.rb
@@ -1,8 +1,9 @@
-require 'guard/kjell/version'
 require 'guard/compat/plugin'
 
 module Guard
   class Kjell < Plugin
+    VERSION = "1.0.2"
+
     def run_all
       puts "Running '#{options[:cmd]}'"
       system options[:cmd]
--- lib/guard/kjell/version.rb
+++
@@ -1,5 +0,0 @@
-module Guard
-  class Kjell
-    VERSION = "1.0.1"
-  end
-end
